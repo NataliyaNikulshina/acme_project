d58bf49332b8275f894cda3c6ba66fd02273b953
Nataliya <nataliya.nikulshina@n-df.ru>
2025-09-20 01:59:46 +0300

    feat: Разграничение доступа ролей

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -7,7 +7,14 @@ from django.urls import reverse_lazy
 from .forms import BirthdayForm
 from .models import Birthday
 from .utils import calculate_birthday_countdown
-from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+
+class OnlyAuthorMixin(UserPassesTestMixin):
+
+    def test_func(self):
+        object = self.get_object()
+        return object.author == self.request.user
 
 
 class BirthdayListView(ListView):
@@ -16,9 +23,15 @@ class BirthdayListView(ListView):
     paginate_by = 10
 
 
-class BirthdayCreateView(CreateView):
+class BirthdayCreateView(LoginRequiredMixin, CreateView):
     model = Birthday
     form_class = BirthdayForm
+
+    def form_valid(self, form):
+        # Присвоить полю author объект пользователя из запроса.
+        form.instance.author = self.request.user
+        # Продолжить валидацию, описанную в форме.
+        return super().form_valid(form) 
 
 
 class BirthdayUpdateView(UserPassesTestMixin, UpdateView):
@@ -29,15 +42,21 @@ class BirthdayUpdateView(UserPassesTestMixin, UpdateView):
     def test_func(self):
         # Получаем текущий объект.
         object = self.get_object()
+        print(object)
+        print(object.author == self.request.user)
         # Метод вернёт True или False. 
         # Если пользователь - автор объекта, то тест будет пройден.
         # Если нет, то будет вызвана ошибка 403.
-        return object.author == self.request.user 
+        return object.author == self.request.user
 
 
-class BirthdayDeleteView(DeleteView):
+class BirthdayDeleteView(UserPassesTestMixin, DeleteView):
     model = Birthday
     success_url = reverse_lazy('birthday:list')
+
+    def test_func(self):
+        object = self.get_object()
+        return object.author == self.request.user
 
 
 class BirthdayDetailView(DetailView):
